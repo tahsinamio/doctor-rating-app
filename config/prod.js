b9237711df8af77332ae06a7a1ff1f3cc11ecdb3
Tahsin Wasit Amio <tahsinamio@gmail.com>
2021-01-12 23:03:23 +0600

    test

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,6 @@
-export const googleClientID = process.env.GOGGLE_CLIENT_ID;
-export const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
-export const mongoURI = process.env.MONGO_URI;
-export const cookieKey = process.env.COOKIE_KEY;
+module.exports = {
+  googleClientID: process.env.GOGGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
